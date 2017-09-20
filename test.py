c824005263f3a7241bcf8a0eb8e0f44b2cca94c7
Tim Eccleston <teccleston@axialhealthcare.com>
2017-09-20 00:31:17 -0500

    add tests for oddpows

--- test.py
+++ test.py
@@ -20,6 +20,9 @@ class RamseyTadpolesTest(unittest.TestCase):
         self.assertEqual(tp.modpows(30, 49), [30, 18, 1])
         self.assertEqual(tp.modpows(30, 50), [30, 0])
 
+    def test030_oddpows(self):
+        self.assertEqual(tp.oddpows([30, 18, 1], 10, 49), [6, 33, 10])
+
 if __name__ == '__main__':
         pdb.set_trace()
         unittest.main()
