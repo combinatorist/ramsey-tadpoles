2b1878ff8b80d7ff15e85722b59c75c569100cd0
Tim Eccleston <timothy.perisho.eccleston+git@gmail.com>
2023-08-14 22:26:30 -0700

    other changes to make circulant script run (with sage)

--- circulant_hamiltonian_chords.sage.py
+++ circulant_hamiltonian_chords.sage.py
@@ -1,39 +1,23 @@
+from sage.all import *
 graphs.CirculantGraph(13,[1,4]).show()
 target = graphs.CirculantGraph(13,[1,4])
-pattern = graphs.Cycle(13)
 pattern = graphs.CycleGraph(13)
 pattern.is_subgraph(target)
 pattern.subgraph_search(target)
 target.subgraph_search(pattern)
-help(target.subgraph_search(pattern))
+#help(target.subgraph_search(pattern))
 result = target.subgraph_search(pattern)
-help(result)
+#help(result)
 dir(result)
 result.edges
 result.edges()
 results = target.subgraph_search_iterator(pattern)
-result = results.next()
-rsults
-results
 result = next(results)
-result.edges
-result
-next(results)
 results = target.subgraph_search_iterator(pattern)
 def generated_chords(generator, nodes):
     double = nodes * 2
     modulus = len(nodes)
     return set((double[i+generator] - double[i]) % modulus for i in range(modulus))
-def all_generated_chords(generator, node_lists):
-    return set().union(generated_chords(node_list) for node_list in node_lists)
-all_generated_chords(results)
-all_generated_chords(4, results)
-def all_generated_chords(generator, node_lists):
-    return set().union(generated_chords(generator, node_list) for node_list in node_lists)
-all_generated_chords(4, results)
-def all_generated_chords(generator, node_lists):
-    return set().union(*(generated_chords(generator, node_list) for node_list in node_lists))
-all_generated_chords(4, results)
 target.subgraph_search_count(pattern)
 def generated_chords(generator, nodes):
     double = nodes + nodes[:generator]
@@ -43,16 +27,7 @@ def generated_chords(generator, nodes):
     double = nodes + nodes[:generator]
     modulus = len(nodes)
     return set((double[i+generator] - double[i]) % modulus for i in range(modulus))
-IPython.terminal.shortcuts.open_input_in_editor
-Sage.terminal.shortcuts.open_input_in_editor
-sage.terminal.shortcuts.open_input_in_editor
-def search_cycles(modulus, root_generator):
-    powers = get_residues(root_generator, modulus)
-    target = graphs.CirculantGraph(modulus, powers)
-    pattern = graphs.CycleGraph(modulus)
-    results = target.subgraph_search_iterator(pattern)
-    return all_generated_chords(root_generator, results)
-search_cycles(13,4)
+#sage.terminal.shortcuts.open_input_in_editor
 def get_residues(generator, modulo):
     residues = [1, generator]
     residue = generator * generator % modulo
@@ -60,40 +35,13 @@ def get_residues(generator, modulo):
             residues.append(residue)
             residue = residue * generator % modulo
     return residues
-search_cycles(13,4)
-def all_generated_chords(generator, node_lists, modulus=None):
-    if not modulus:
-        start = next(node_lists)
-        modulus = len(start)
-        node_lists = itertools.chain([start], node_lists)
-    chords = set()
-    for node_list in node_lists:
-        chords.union(generated_chords(generator, node_list))
-        if len(chords) + 1 == modulus:
-            break
-    return chords
-search_cycles(13,4)
 import itertools
-search_cycles(13,4)
 def search_cycles(modulus, root_generator):
     powers = get_residues(root_generator, modulus)
     target = graphs.CirculantGraph(modulus, powers)
     pattern = graphs.CycleGraph(modulus)
     results = target.subgraph_search_iterator(pattern)
     return all_generated_chords(root_generator, results, modulus)
-search_cycles(13,4)
-def all_generated_chords(generator, node_lists, modulus=None):
-    if not modulus:
-        start = next(node_lists)
-        modulus = len(start)
-        node_lists = itertools.chain([start], node_lists)
-    chords = set()
-    for node_list in node_lists:
-        chords.extend(generated_chords(generator, node_list))
-        if len(chords) + 1 == modulus:
-            break
-    return chords
-search_cycles(13,4)
 def all_generated_chords(generator, node_lists, modulus=None):
     if not modulus:
         start = next(node_lists)
@@ -107,167 +55,14 @@ def all_generated_chords(generator, node_lists, modulus=None):
     return chords
 search_cycles(13,4)
 search_cycles(13,4)
-def search_examples():
-    modulus = 3
-    while True:
-        modulus += 1
-        for root_generator in range(2, modulus - 1):
-            result = search_cycles(modulus, root_generator)
-            if len(result) == modulus - 1:
-                print(f'm:{modulus}, r:{root_generator}: all found')
-            else:
-                print(f'm:{modulus}, r:{root_generator}: {modulus - 1 - len(result)} missing')
-                print(result)
-search_examples()
-def search_examples():
-    modulus = 3
-    while True:
-        modulus += 1
-        for root_generator in range(2, modulus - 1):
-            if not coprime(root_generator, modulus):
-                continue
-            result = search_cycles(modulus, root_generator)
-            if len(result) == modulus - 1:
-                print(f'm:{modulus}, r:{root_generator}: all found')
-            else:
-                print(f'm:{modulus}, r:{root_generator}: {modulus - 1 - len(result)} missing')
-                print(result)
-#%edit
-search_examples()
 def coprime(k,l):
     return gcd(k,l) == 1
 coprime(13,4)
 coprime(15,5)
-search_examples()
 #%store search_examples() > ramsey-results.txt
-def search_examples(fileprefix='~/ramsey-results.txt'):
-  with open(fileprefix + now(), 'w') as f:
-    modulus = 3
-    while True:
-        modulus += 1
-        for root_generator in range(2, modulus - 1):
-            if not coprime(root_generator, modulus):
-                continue
-            result = search_cycles(modulus, root_generator)
-            if len(result) == modulus - 1:
-                f.write(f'm:{modulus}, r:{root_generator}: all found')
-            else:
-                f.write(f'm:{modulus}, r:{root_generator}: {modulus - 1 - len(result)} missing')
-                f.write(result)
-search_examples()
-from datetime.datetime import now
-from datetime import now
 from datetime import datetime
-def search_examples(fileprefix='~/ramsey-results.txt'):
-  with open(fileprefix + datetime.now(), 'w') as f:
-    modulus = 3
-    while True:
-        modulus += 1
-        for root_generator in range(2, modulus - 1):
-            if not coprime(root_generator, modulus):
-                continue
-            result = search_cycles(modulus, root_generator)
-            if len(result) == modulus - 1:
-                f.write(f'm:{modulus}, r:{root_generator}: all found')
-            else:
-                f.write(f'm:{modulus}, r:{root_generator}: {modulus - 1 - len(result)} missing')
-                f.write(result)
-search_examples()
-def search_examples(fileprefix='~/ramsey-results.txt'):
-  with open(fileprefix + str(datetime.now()), 'w') as f:
-    modulus = 3
-    while True:
-        modulus += 1
-        for root_generator in range(2, modulus - 1):
-            if not coprime(root_generator, modulus):
-                continue
-            result = search_cycles(modulus, root_generator)
-            if len(result) == modulus - 1:
-                f.write(f'm:{modulus}, r:{root_generator}: all found')
-            else:
-                f.write(f'm:{modulus}, r:{root_generator}: {modulus - 1 - len(result)} missing')
-                f.write(result)
-search_examples()
-def search_examples(fileprefix='~/ramsey-results.txt'):
-  with open(fileprefix + str(today()), 'w') as f:
-    modulus = 3
-    while True:
-        modulus += 1
-        for root_generator in range(2, modulus - 1):
-            if not coprime(root_generator, modulus):
-                continue
-            result = search_cycles(modulus, root_generator)
-            if len(result) == modulus - 1:
-                f.write(f'm:{modulus}, r:{root_generator}: all found')
-            else:
-                f.write(f'm:{modulus}, r:{root_generator}: {modulus - 1 - len(result)} missing')
-                f.write(result)
-from datetime import today
 from datetime import date
-from date import today
-today = date.today
-def search_examples(fileprefix='~/ramsey-results.txt'):
-  with open(fileprefix + str(today()), 'w') as f:
-    modulus = 3
-    while True:
-        modulus += 1
-        for root_generator in range(2, modulus - 1):
-            if not coprime(root_generator, modulus):
-                continue
-            result = search_cycles(modulus, root_generator)
-            if len(result) == modulus - 1:
-                f.write(f'm:{modulus}, r:{root_generator}: all found')
-            else:
-                f.write(f'm:{modulus}, r:{root_generator}: {modulus - 1 - len(result)} missing')
-                f.write(result)
-search_examples()
-def search_examples(fileprefix='ramsey-results.txt'):
-  with open(fileprefix + str(now()), 'w') as f:
-    modulus = 3
-    while True:
-        modulus += 1
-        for root_generator in range(2, modulus - 1):
-            if not coprime(root_generator, modulus):
-                continue
-            result = search_cycles(modulus, root_generator)
-            if len(result) == modulus - 1:
-                f.write(f'm:{modulus}, r:{root_generator}: all found')
-            else:
-                f.write(f'm:{modulus}, r:{root_generator}: {modulus - 1 - len(result)} missing')
-                f.write(result)
-search_examples()
 now = datetime.now
-search_examples()
-def search_examples(fileprefix='ramsey-results.txt'):
-  with open(fileprefix + str(now()), 'w') as f:
-    modulus = 3
-    while True:
-        modulus += 1
-        for root_generator in range(2, modulus - 1):
-            if not coprime(root_generator, modulus):
-                continue
-            result = search_cycles(modulus, root_generator)
-            if len(result) == modulus - 1:
-                f.write(f'm:{modulus}, r:{root_generator}: all found')
-            else:
-                f.write(f'm:{modulus}, r:{root_generator}: {modulus - 1 - len(result)} missing')
-                f.write(sorted(result))
-search_examples()
-def search_examples(fileprefix='ramsey-results.txt'):
-  with open(fileprefix + str(now()), 'w') as f:
-    modulus = 3
-    while True:
-        modulus += 1
-        for root_generator in range(2, modulus - 1):
-            if not coprime(root_generator, modulus):
-                continue
-            result = search_cycles(modulus, root_generator)
-            if len(result) == modulus - 1:
-                f.write(f'm:{modulus}, r:{root_generator}: all found')
-            else:
-                f.write(f'm:{modulus}, r:{root_generator}: {modulus - 1 - len(result)} missing')
-                f.write(str(sorted(result)))
-search_examples()
 #%edit
 def search_examples(fileprefix='ramsey-results.txt'):
   with open(fileprefix + str(now()), 'w') as f:
@@ -283,17 +78,6 @@ def search_examples(fileprefix='ramsey-results.txt'):
             else:
                 f.write(f'm:{modulus}, r:{root_generator}: {modulus - 1 - len(result)} missing')
                 f.write(str(sorted(result)))
-                q
-#%edit
-search_examples()
-#%edit search_examples
-search_examples()
-#%edit search_examples
-#%edit search_examples
-search_examples()
-#%edit search_examples
-search_examples()
-#%edit search_examples
 search_examples()
 #%edit search_examples
 search_examples()
