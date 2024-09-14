eb03ae05fcd767e324f1db997b2e75378417586c
Tim Eccleston <timothy.perisho.eccleston+git@gmail.com>
2024-09-14 07:19:32 -0700

    avoid OOM

--- spark/Main.scala
+++ spark/Main.scala
@@ -71,7 +71,7 @@ object Main {
 
 final case class WithSpark(spark: SparkSession, modulo: Int, mainSeed: Int) {
   val partitions = 15
-  val N = 2 * pure.F.factorial(11)
+  val N = pure.F.factorial(10)
   import spark.implicits._
 
   def fromScratch =
