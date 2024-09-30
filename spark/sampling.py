14299877747b2a77e281c574605b1ccf71fb39f3
Tim Eccleston <timothy.perisho.eccleston+git@gmail.com>
2024-09-29 21:06:47 -0700

    base min/max modulo relationship on relative size, not starting point

--- spark/sampling.py
+++ spark/sampling.py
@@ -17,8 +17,7 @@ def run(starting_max_modulo=31, starting_min_modulo=31, starting_modulo=31):
     for modulo in range(starting_modulo, max_modulo + 1):
       print(f'running with ${(max_modulo, min_modulo, modulo)}')
       system(f'spark-submit --class Main target/scala-2.13/spark-ramsey-numbers_2.13-0.1.jar ${modulo}')
-    if log2(max_modulo) % 1 == 0:
-      min_modulo += 1
+    min_modulo = max(starting_min_modulo, log2(max_modulo))
     max_modulo += 1
 
 if __name__ == '__main__':
